fdf879c8fa8a5b03fdd4f1eff0bd8fd895ac130a
jake lamers <jakelamers931@gmail.com>
2021-09-21 23:10:00 -0500

    Added + button functionality

--- app/src/main/java/com/example/basiccalc/MainActivity.java
+++ app/src/main/java/com/example/basiccalc/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.basiccalc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunction(View view) {
+        EditText firstNumText = (EditText) findViewById(R.id.firstNum);
+
+        Log.i("entered",firstNumText.getText().toString());
+
+        //Toast.makeText(MainActivity.this,, Toast.LENGTH_LONG).show();
+    }
+
+    public void additionClick(View view) {
+        int sum;
+
+        EditText firstNumText = (EditText) findViewById(R.id.firstNum);
+        EditText secondNumText = (EditText) findViewById(R.id.secondNum);
+
+        sum = Integer.parseInt(firstNumText.getText().toString()) + Integer.parseInt(secondNumText.getText().toString());
+
+        goToActivity2(sum);
+    }
+
+    public void goToActivity2(int answer) {
+        Intent intent = new Intent(this,MainActivity2.class);
+        intent.putExtra("message",String.valueOf(answer));
+        startActivity(intent);
     }
 }
